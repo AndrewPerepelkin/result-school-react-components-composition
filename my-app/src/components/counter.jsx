6ae3cec54617fc9389d9f32c7b73d5258aabc8ec
AndrewPerepelkin <avp.tep@rambler.ru>
2022-09-16 18:31:22 +0300

    refactor: desructurisation props in counter

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-const Counter = (props) => {
-    const {value} = props;
-
+const Counter = ({id, value, name, onIncrement, onDecrement, onDelete}) => {
     const formatValue = () => {
         return value === 0 ? "empty" : value;
     };
@@ -13,23 +11,23 @@ const Counter = (props) => {
 
     return (
         <div>
-            <span>{props.name}</span>
+            <span>{name}</span>
             <span className={getBageClasses()}>{formatValue()}</span>
             <button
                 className='btn btn-primary btn-sm m-2'
-                onClick={() => props.onIncrement(props.id)}
+                onClick={() => onIncrement(id)}
             >
                 +
             </button>
             <button
                 className='btn btn-primary btn-sm m-2'
-                onClick={() => props.onDecrement(props.id)}
+                onClick={() => onDecrement(id)}
             >
                 -
             </button>
             <button
                 className='btn btn-danger btn-sm m-2'
-                onClick={() => props.onDelete(props.id)}
+                onClick={() => onDelete(id)}
             >
                 Удалить
             </button>
